a624d1245475e5f558e6c01ec9e2d792e5a51a0f
Hannah <hannah_wight@hotmail.com>
2014-07-14 16:10:51 +0100

    updated README.  Now trying to remember how to use rspec...

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'pages/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'home#index'
+root 'pages#home'
+
+get '/about' => 'pages#about'
+get '/contact' => 'pages#contact'
+get '/news' => 'pages#news'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
